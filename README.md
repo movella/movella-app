1f799059a1c0c4403d2bed04736da623478f86cc
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2021-12-25 20:05:54 -0300

    changes, features

--- README.md
+++ README.md
@@ -4,6 +4,17 @@
 
 ## Features
 
+* feedback (missing)
+* provider (missing)
+* http (missing)
+* socket.io (missing)
+* rent furniture (missing)
+* put furniture for rent (missing)
+* verify account (missing)
+* add keys (missing)
+* history (missing)
+* my furniture (missing)
+
 ### Splash page
 
 * loads
@@ -22,8 +33,25 @@
 ### Main page
 
 * welcome dialog
+* version info
+* drawer info (missing)
 * statistics (missing)
 * about (missing)
 * help (missing)
 * my account (missing)
 * my furniture (missing)
+* sign out
+* sign out working (missing)
+* recommended furniture
+* recommended furniture cards (missing)
+* recommended furniture navigation (missing)
+* recommended furniture data (missing)
+* unverified account card
+* unverified account card working (missing)
+* search for furniture
+* search for furniture working (missing)
+* history (missing)
+
+### My account page (missing)
+
+### My furniture page (missing)
--- lib/core/main_page.dart
+++ lib/core/main_page.dart
@@ -6,6 +6,7 @@ import 'package:movella_app/core/splash_page.dart';
 import 'package:movella_app/utils/services/shared_preferences.dart';
 import 'package:movella_app/widgets/custom_icon.dart';
 import 'package:movella_app/widgets/custom_spacer.dart';
+import 'package:package_info/package_info.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -56,7 +57,7 @@ class _MainPageState extends State<MainPage> {
                           style: Theme.of(context).textTheme.caption,
                         ),
                         Text(
-                          'Membro desde 12/12/2021',
+                          '${AppLocalizations.of(context)!.memberSince} 12/12/2021',
                           style: Theme.of(context).textTheme.caption,
                         ),
                       ],
@@ -81,6 +82,51 @@ class _MainPageState extends State<MainPage> {
                     leading: const Icon(MdiIcons.help),
                     onTap: () {
                       // TODO: fix
+                    },
+                  ),
+                  FutureBuilder<PackageInfo>(
+                    future: PackageInfo.fromPlatform(),
+                    builder: (context, snapshot) {
+                      final data = snapshot.data;
+
+                      if (snapshot.hasError)
+                        return Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Text(
+                            AppLocalizations.of(context)!.somethingWentWrong,
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context).textTheme.caption,
+                          ),
+                        );
+
+                      if (snapshot.connectionState == ConnectionState.done &&
+                          data != null)
+                        return Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Text(
+                                '${data.appName} ${data.version}',
+                                textAlign: TextAlign.center,
+                                style: Theme.of(context).textTheme.caption,
+                              ),
+                              Text(
+                                'Build ${data.buildNumber}',
+                                textAlign: TextAlign.center,
+                                style: Theme.of(context).textTheme.caption,
+                              ),
+                            ],
+                          ),
+                        );
+
+                      return Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: const Center(
+                          child: const CircularProgressIndicator(),
+                        ),
+                      );
                     },
                   ),
                 ],
@@ -123,8 +169,10 @@ class _MainPageState extends State<MainPage> {
                         context: context,
                         builder: (context) {
                           return AlertDialog(
-                            title: const Text('Atenção'),
-                            content: const Text('Deseja sair de sua conta?'),
+                            title: Text(AppLocalizations.of(context)!.signOut),
+                            content: Text(
+                              AppLocalizations.of(context)!.signOutOfAccount,
+                            ),
                             actions: [
                               TextButton(
                                 child: Text(AppLocalizations.of(context)!.no),
@@ -155,15 +203,171 @@ class _MainPageState extends State<MainPage> {
         ),
       ),
       body: SafeArea(
-        child: ListView(
+        child: Stack(
+          alignment: Alignment.bottomCenter,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Text(
-                'Boa tarde, Usuário!',
-                style: Theme.of(context).textTheme.headline6,
-              ),
+            ListView(
+              padding: const EdgeInsets.only(bottom: 100),
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Text(
+                    '${_getGreetingString(context)}, Usuário!',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Text(
+                    AppLocalizations.of(context)!.recommended,
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                ),
+                SingleChildScrollView(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: List.generate(7, (index) {
+                      return SizedBox(
+                        width: 160,
+                        child: Card(
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          child: InkWell(
+                            onTap: () {
+                              // TODO: fix
+                            },
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                Image.network(
+                                  'https://via.placeholder.com/100',
+                                  fit: BoxFit.cover,
+                                  height: 100,
+                                ),
+                                // TODO: fix
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                    top: 8,
+                                    left: 8,
+                                    right: 8,
+                                  ),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.stretch,
+                                    children: [
+                                      // TODO: fix
+                                      const Text('Cadeira'),
+                                      RichText(
+                                        text: TextSpan(
+                                          children: [
+                                            TextSpan(
+                                              // TODO: fix
+                                              text: 'R\$ 23,00',
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .bodyText2,
+                                            ),
+                                            TextSpan(
+                                              // TODO: fix
+                                              text:
+                                                  ' ${AppLocalizations.of(context)!.perMonth}',
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .caption
+                                                  ?.apply(fontSizeFactor: .8),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      Text(
+                                        // TODO: fix
+                                        'Elit et laboris sint proident laboris incididunt amet et. Excepteur cillum non sunt sint ex elit quis velit pariatur.',
+                                        maxLines: 4,
+                                        overflow: TextOverflow.ellipsis,
+                                        style:
+                                            Theme.of(context).textTheme.caption,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                const Divider(),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                    left: 8,
+                                    right: 8,
+                                    bottom: 8,
+                                  ),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.stretch,
+                                    children: [
+                                      // TODO: fix
+                                      const Text('Usuário 2'),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+                Dismissible(
+                  key: const ValueKey('dismissible'),
+                  child: Card(
+                    margin: const EdgeInsets.only(
+                      top: 16,
+                      left: 16,
+                      right: 16,
+                      bottom: 8,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(AppLocalizations.of(context)!
+                              .verifyAccountMessage),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8),
+                            child: OutlinedButton(
+                              child: Text(
+                                AppLocalizations.of(context)!.goToMyAccount,
+                              ),
+                              onPressed: () {
+                                // TODO: fix
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: ElevatedButton.icon(
+                      icon: const Icon(MdiIcons.magnify),
+                      label: Text(
+                        AppLocalizations.of(context)!.searchForFurniture,
+                        textAlign: TextAlign.center,
+                      ),
+                      onPressed: () {
+                        // TODO: fix
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
@@ -245,4 +449,14 @@ class _WelcomeDialogWidgetState extends State<WelcomeDialogWidget> {
       ),
     );
   }
+}
+
+String _getGreetingString(BuildContext context) {
+  final hour = DateTime.now().hour;
+
+  if (hour >= 18) return AppLocalizations.of(context)!.goodEvening;
+
+  if (hour >= 12) return AppLocalizations.of(context)!.goodAfternoon;
+
+  return AppLocalizations.of(context)!.goodMorning;
 }
